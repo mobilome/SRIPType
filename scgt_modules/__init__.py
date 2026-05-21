91d4ef6c6cff1a4c0982cc8c2228a713e455703d
mobilome <3298990@qq.com>
2026-05-21 23:25:20 +0800

    change the version to 1.0.0

--- scgt_modules/__init__.py
+++ scgt_modules/__init__.py
@@ -1,4 +1,4 @@
 """scgt - SINE-RIP chip genotyper."""
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 __author__ = "Mobilome Team"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="scgt",
-    version="0.1.0",
+    version="1.0.0",
     packages=find_packages(include=["scgt_modules", "scgt_modules.*"]),
     package_data={
         "scgt_modules": ["data/*"],
